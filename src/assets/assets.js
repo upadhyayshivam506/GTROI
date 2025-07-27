d78e06f94728c02a1edbb7d21280efccb722c74b
shivam upadhyay <upadhyayshivam506@gmail.com>
2025-07-27 20:25:56 +0530

    Your commit message here

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -4,6 +4,15 @@ import vector1 from './vector-1.png'
 import vector2 from './vector-2.png'
 import background from'./background.png'
 import logo from './logo.png'
+import nikebg from  './nikebg.jpg'
+import CHALLENGE from './challenges.png'
+import STORE from './store.png'
+import REGIMES from './regimes.png'
+import GOALS from './goals.png'
+import PROGRESS from './progress.png'
+import GREEN_ICON from './GREEN_ICON.png'
+import YELLOW_ICON from './YELLOW_ICON.png'
+import setting from './setting.png'
 export const assets ={
     layer1,
     layer2,
@@ -11,5 +20,14 @@ export const assets ={
     vector2,
     background,
     logo,
+    nikebg,
+    CHALLENGE,
+    STORE,
+    REGIMES,
+    GOALS,
+    PROGRESS,
+    YELLOW_ICON,
+    GREEN_ICON,
+    setting,
 
 };
--- src/pages/Nike.jsx
+++ src/pages/Nike.jsx
@@ -1,35 +1,62 @@
 import './Nike.css';
+import { assets } from '../assets/assets';
 
 function Nike() {
   return (
     <div className="Nike-box">
       <div className="nike-container">
-        <div className="top-bar">
-          <div className="gear-icon" />
-          <div className="currency">
-            <span className="green">🟩 14</span>
-            <span className="yellow">🟨 150</span>
+        
+        {/* Top Nav Bar */}
+        <div className="nav-bar">
+          <div className="nav-left">
+            <img src={assets.setting} alt="Gear" className="gear-icon" />
+          </div>
+          <div className="nav-right">
+            <div className="currency-item green">
+              <img src={assets.GREEN_ICON} alt="Green" />
+              <span>500</span>
+            </div>
+            <div className="currency-item yellow">
+              <img src={assets.YELLOW_ICON} alt="Yellow" />
+              <span>150</span>
+            </div>
           </div>
         </div>
 
+        {/* Hero Text */}
         <div className="hero-text">
           <h1>NIKE GEAR<br />@ 30% OFF</h1>
           <p>EXCLUSIVELY ON GTROI</p>
         </div>
 
+        {/* Bottom Menu */}
         <div className="menu-bar">
-          <button>CHALLENGES</button>
-          <button>REGIMES</button>
-          <button>GOALS</button>
-          <button>PROGRESS</button>
-          <button className="active">STORE</button>
+          <button className="menu-button">
+            <img src={assets.CHALLENGE} alt="Challenges" />
+            <span>CHALLENGES</span>
+          </button>
+          <button className="menu-button">
+            <img src={assets.REGIMES} alt="Regimes" />
+            <span>REGIMES</span>
+          </button>
+          <button className="menu-button">
+            <img src={assets.GOALS} alt="Goals" />
+            <span>GOALS</span>
+          </button>
+          <button className="menu-button">
+            <img src={assets.PROGRESS} alt="Progress" />
+            <span>PROGRESS</span>
+          </button>
+          <button className="menu-button active">
+            <img src={assets.STORE} alt="Store" />
+            <span>STORE</span>
+          </button>
         </div>
+
       </div>
     </div>
   );
 }
-
-
 
 export default Nike;
 
